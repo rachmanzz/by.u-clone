1221d859d0b2362d2777314925c6c5332d31261a
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-08-17 15:39:15 +0800

    test tsx resources

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+
+const App = () => {
+  return <SafeAreaView><Text>This is work</Text></SafeAreaView>
+};
+
+
+export default App;
