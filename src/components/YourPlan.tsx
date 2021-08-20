2d2235a62dd41d8bf44f718fa086eafab1bc7f23
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-08-20 18:06:10 +0800

    update uplan

--- src/components/YourPlan.tsx
+++ src/components/YourPlan.tsx
@@ -3,11 +3,14 @@ import {View, ViewStyle, StatusBar, Text, useWindowDimensions} from "react-nativ
 import SafeArea from "./IosSafeArea";
 import Liners from "./svg/Liners";
 import TextField from "./ui/TextField";
-import Svg, { Path } from "react-native-svg";
+import { SvgXml } from "react-native-svg";
+import ToppingIcon from "../assets/img/ToppingIcon.svg"
 import DataIcon from "./svg/icons/DataIcon";
 import CardField from "./ui/CardField";
 import ArrowLeftAlt from "./svg/icons/ArrowLeftAlt";
 import * as Progress from 'react-native-progress';
+import PulsaIcon from "./svg/icons/PulsaIcon";
+import TelephoneIcon from "./svg/icons/TelephoneIcon";
 
 
 export default function YourPlan ({style}:{style: ViewStyle}) {
@@ -36,12 +39,12 @@ export default function YourPlan ({style}:{style: ViewStyle}) {
                                 </View>
                                 <View style={{backgroundColor: "#DDE2E8", height: '100%', width: 1}}/>
                                 <View style={{justifyContent: "center", alignItems: "center"}}>
-                                    <DataIcon width={35} height={35}/>
+                                    <SvgXml width={35} height={35} xml={ToppingIcon} /> 
                                     <TextField style={{marginTop: 5}}>Beli Topping</TextField>
                                 </View>
                                 <View style={{backgroundColor: "#DDE2E8", height: '100%', width: 1}}/>
                                 <View style={{justifyContent: "center", alignItems: "center"}}>
-                                    <DataIcon width={35} height={35}/>
+                                    <PulsaIcon width={35} height={35}/>
                                     <TextField style={{marginTop: 5}}>Beli Pulsa</TextField>
                                 </View>
                             </View>
@@ -50,41 +53,53 @@ export default function YourPlan ({style}:{style: ViewStyle}) {
                             <CardField style={{padding: 10}}>
                                 <View>
                                     <View style={{ flexDirection: "row", justifyContent: "space-between", marginLeft: 5, marginRight: 5 }}>
-                                        <TextField size={14} type="bold">Rincian pemakaian</TextField>
+                                        <TextField size={16} type="bold">Rincian pemakaian</TextField>
                                         <View style={{flexDirection: "row", alignItems: 'center', justifyContent: "center"}}>
                                             <TextField style={{marginRight: 5}}>Lihat Detail</TextField>
                                             <ArrowLeftAlt color="#0BB8E9"/>
                                         </View>
                                     </View>
-                                    <View style={{marginTop: 13}}>
-                                        <View style={{flexDirection: "row", alignItems: 'center', alignContent: 'center', marginBottom: 15}}>
+                                    <View style={{marginTop: 30, paddingRight: 10}}>
+                                        <View style={{flexDirection: "row", alignItems: 'center', alignContent: 'center', marginBottom: 30}}>
                                             <DataIcon />
                                             <View style={{paddingLeft: 10, flexGrow: 1}}>
-                                                <View style={{flexDirection: "row", justifyContent: "space-between"}}>
-                                                    <TextField>Sisa Total Data</TextField>
-                                                    <TextField>0 GB</TextField>
+                                                <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 5}}>
+                                                    <TextField type="thin" size={12}>Sisa Total Data</TextField>
+                                                    <View style={{flexDirection: "row"}}>
+                                                        <TextField size={13} style={{marginRight: 3}} color="#AF173D" type="medium">5</TextField>
+                                                        <TextField size={13} type="medium">GB</TextField>
+                                                    </View>
+                                                    
                                                 </View>
-                                                <Progress.Bar progress={0.3} width={null} />
+                                                <Progress.Bar progress={0.5} width={null} borderColor="#DDE2E8" height={7} style={{backgroundColor: "#DDE2E8"}} />
                                             </View>
                                         </View>
-                                        <View style={{flexDirection: "row", alignItems: 'center', alignContent: 'center', marginBottom: 15}}>
-                                            <DataIcon />
+                                        <View style={{flexDirection: "row", alignItems: 'center', alignContent: 'center', marginBottom: 30}}>
+                                            <SvgXml width={24} height={24} xml={ToppingIcon} /> 
                                             <View style={{paddingLeft: 10, flexGrow: 1}}>
-                                                <View style={{flexDirection: "row", justifyContent: "space-between"}}>
-                                                    <TextField>Sisa Topping</TextField>
-                                                    <TextField>0 GB</TextField>
+                                                <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 5}}>
+                                                    <TextField type="thin" size={12}>Sisa Topping</TextField>
+                                                    <View style={{flexDirection: "row"}}>
+                                                        <TextField size={13} style={{marginRight: 3}} color="#AF173D" type="medium">2</TextField>
+                                                        <TextField size={13} type="medium">GB</TextField>
+                                                    </View>
+                                                    
                                                 </View>
-                                                <Progress.Bar progress={0.3} width={null} />
+                                                <Progress.Bar progress={0.3} width={null} borderColor="#DDE2E8" height={7} style={{backgroundColor: "#DDE2E8"}} />
                                             </View>
                                         </View>
                                         <View style={{flexDirection: "row", alignItems: 'center', alignContent: 'center'}}>
-                                            <DataIcon />
+                                            <TelephoneIcon  />
                                             <View style={{paddingLeft: 10, flexGrow: 1}}>
-                                                <View style={{flexDirection: "row", justifyContent: "space-between"}}>
-                                                    <TextField>Sisa Telfon</TextField>
-                                                    <TextField>0 Menit</TextField>
+                                                <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 5}}>
+                                                    <TextField type="thin" size={12}>Sisa Telfon</TextField>
+                                                    <View style={{flexDirection: "row"}}>
+                                                        <TextField size={13} style={{marginRight: 3}} color="#AF173D" type="medium">99</TextField>
+                                                        <TextField size={13} type="medium">Menit</TextField>
+                                                    </View>
+                                                    
                                                 </View>
-                                                <Progress.Bar progress={0.3} width={null} />
+                                                <Progress.Bar progress={0.8} width={null} borderColor="#DDE2E8" height={7} style={{backgroundColor: "#DDE2E8"}} />
                                             </View>
                                         </View>
                                     </View>
